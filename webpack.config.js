c53e5203727ec7ebddcf6e21de065106e675d410
Venomkajo <kajetanrespondek@gmail.com>
2024-04-08 00:43:00 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML template
+      filename: 'index.html' // Output HTML filename
+    }),
+    new FaviconsWebpackPlugin('./src/favi.png') // Path to your favicon
+  ]
+};
